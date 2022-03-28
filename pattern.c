dd15d21066dd3f2ac26bcc7b86ac31e1953ef519
Afrin-S <93669247+Afrin-S@users.noreply.github.com>
2022-03-28 14:46:11 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int row,col,inp;
+    scanf("%d",&inp);
+    for(row=1;row<=inp;row++)
+    {
+        for(col=0;col<=inp;col++)
+        
+        {
+            printf("*");
+        }
+            printf("\n");
+    }
+      return 0;
+}
